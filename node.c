a08f0b8ccc037497045159c5a8870e432e002d71
sdutton <stephen.dutton.mail@me.com>
2023-02-14 17:29:09 +0100

    node.h structures completed

---
+++ node.c
@@ -0,0 +1,6 @@
+//
+// Created by sdutton on 14.02.23.
+//
+#include "node.h"
+
+
--- node.h
+++ node.h
@@ -1,14 +1,30 @@
 
-typedef float (*regression) (float);
-typedef float (*sigmoid) (float);
-typedef float (*relu) (float);
-typedef float (*softmax) (float);
-typedef float* (*chain)(float*);
+#define LEARNING_RATE 0.5f;
+
+enum layerType {input,hidden, output};
 
 typedef struct nodeDef{
+    float* learningRate;
     float** inputs;
     float** weights;
-    chain _chain;
-    relu output;
+    float (*regression) (float);
 } node, *pNode;
+
+typedef struct layerDef
+{
+    int layNumber;
+    enum layerType _layerType;
+    pNode* nodes;
+} layer, *pLayer;
+
+typedef struct networkDef{
+    char* restrict name;
+    pLayer* allLayers;
+    pLayer input;
+    pLayer output;
+} network, *pNetwork;
+
+pNode createNode();
+pLayer createLayer(int count, enum  layerType _layerType, pLayer proceedingLayer);
+pNetwork createNetwork(char* restrict name, pLayer* layers);
 
